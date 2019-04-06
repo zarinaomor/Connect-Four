373a0b0b86c660c014d9ccd347ae0d7b7fbf0a6c
zarinaomor <zarinaomorova@yahoo.com>
2019-04-05 23:18:20 -0700

    declared function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log('test');
+const columns = 7;
+const rows = 6;
+const matchToWin = 4;
+
+const playerTurn = 0;
+const grid = [[], [], [], [], [], []];
+const gameOver = false;
+
+function updateMatches(value, matches) {
+    return value === playerTurn ? (matches || 0) + 1 : 0
+  }
