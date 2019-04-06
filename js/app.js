f25046c2f997b5b245a87d43d3d803c389d9ca15
zarinaomor <zarinaomorova@yahoo.com>
2019-04-05 23:34:56 -0700

    created eventListener

--- js/app.js
+++ js/app.js
@@ -9,3 +9,9 @@ const gameOver = false;
 function updateMatches(value, matches) {
     return value === playerTurn ? (matches || 0) + 1 : 0
   }
+
+  const table = document.querySelector('table');
+  table.addEventListener('click', function(e){
+    let column = e.target.cell;
+    console.log('clicked');
+  })
